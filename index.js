1908e06d0e7c3bbb3ed3cabc3c8b736922f31493
Eric Monné Mesalles <emonne97@gmail.com>
2019-07-10 09:58:42 +0200

    10 July

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var lgAssistantApi;
+const {dialogflow, SimpleResponse} = require('actions-on-google')
+const express = require('express')
+const bodyParser = require('body-parser')
+const { exec } = require('child_process');
+const app = dialogflow()
 
+app.intent('Fly',function(conv){
+  console.log(conv.parameters)
+  location = "search="+conv.parameters['geo-city']
+  response = new SimpleResponse({
+    text: "flying to " + conv.parameters['geo-city'],
+    speach: "flying to " + conv.parameters['geo-city']
+  })
+  conv.ask( response )
+})
 
+app.intent('Find a plane',function(req,res){
+  console.log(req.parameters)
+})
 
-
-
-
-module.exports = lgAssistantApi;
+const expressApp = express().use(bodyParser.json())
+expressApp.post('/', app)
+expressApp.listen(8080)
